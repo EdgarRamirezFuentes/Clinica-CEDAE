a82c1373a756819b338a75febf9de9524fc33c86
Edgar Alejandro Ramírez Fuentes <edgar-alejandro-fuentes@hotmail.com>
2021-01-19 17:40:35 -0600

    Creacion de cita listo

--- applications/gestion_recepcion/models.py
+++ applications/gestion_recepcion/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class Recepcionista(models.Model):
-    empleado_id = models.ForeignKey('gestion_empleados.Empleado', on_delete=models.SET_NULL, null=True)
+    empleado_id = models.ForeignKey('gestion_empleados.Empleado', primary_key=True, on_delete=models.CASCADE)
--- applications/gestion_recepcion/views.py
+++ applications/gestion_recepcion/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView
 from django.contrib import messages
 import datetime
-from datetime import date, datetime
+from datetime import date, datetime, time, timedelta
 from ..gestion_pacientes.models import Consulta
 from ..gestion_pacientes.models import Cita
 from ..gestion_farmacia.models import Ticket
 from ..gestion_pacientes.models import Paciente
 from ..gestion_medicos.models import Medico
+from ..gestion_expediente.models import Expediente
 
 class Home(TemplateView):
     template_name = 'gestion_recepcion/home.html'
@@ -17,6 +18,40 @@ class primera_citar(TemplateView):
 
 class nueva_cita(TemplateView):
     template_name = 'gestion_recepcion/nueva_cita.html'
+    
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            formato = "%H:%M:%S"
+            curpPaciente = request.POST['curp']
+            costo = request.POST['costo']
+            fecha = request.POST['fecha']
+            medico = Medico.objects.get(empleado_id_id = request.POST['medico'])
+            hora =  request.POST['hora'] + ":00"
+            h1 = datetime.strptime(hora, formato)
+            paciente = Paciente.objects.get(idUsuario_id=curpPaciente)
+            expediente = Expediente.objects.get(paciente_curp = paciente)
+            horaFin = str(h1 + timedelta(minutes=30)).split(" ")[1]
+            nuevaCita = Cita (
+                paciente_id = paciente,
+                medico_id = medico,
+                fecha = fecha,
+                hora_inicio = hora,
+                hora_fin = horaFin
+            )
+            nuevaCita.save()
+            nuevaConsulta = Consulta(
+                cita_id = Cita.objects.all().last(),
+                expediente_id = expediente,
+                costo = costo
+            )
+            nuevaConsulta.save()
+            
+        return redirect('agenda')
+    
+    def get_context_data(self, **kwargs):
+        context = super(nueva_cita, self).get_context_data(**kwargs)
+        context['medicos'] = Medico.objects.all().order_by('empleado_id__usuario_curp__apellidoPaterno')
+        return context
 
 class agenda(TemplateView):
     template_name ='gestion_recepcion/agenda.html'
