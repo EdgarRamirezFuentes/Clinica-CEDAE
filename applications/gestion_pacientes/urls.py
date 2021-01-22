4cc7622e1898e71e97992a5ef457a9859645658d
LuisAHC <luis.h.ceciliano@gmail.com>
2021-01-22 00:45:22 -0500

    listo modulo de paciente, faltan validaciones

--- applications/gestion_pacientes/urls.py
+++ applications/gestion_pacientes/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('paciente/', views.Home.as_view(), name='homePaciente'),
-    path('medico/perfil/<pk>/', views.verPerfil.as_view(), name='patientProfile'),
-    path('medico/expediente/<pk>/', views.verExpediente.as_view(), name='medicalRecord'),
+    path('paciente/perfil/<pk>/', views.verPerfilPaciente.as_view(), name='patientProfilePatient'),
+    path('paciente/expediente/<pk>/', views.verExpediente.as_view(), name='medicalRecord'),
+    path('paciente/contraseña/', views.cambiarContraseña.as_view(), name='changePassword'),
+    path('paciente/cambiarContraseña/<pk>', views.nuevaContraseña.as_view(), name='newPassword'),
 ]
--- applications/gestion_pacientes/views.py
+++ applications/gestion_pacientes/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from django.views.generic import TemplateView, ListView, DetailView, UpdateView
+import datetime
+from datetime import date
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 from ..gestion_pacientes.models import Paciente 
 from ..gestion_usuarios.models import Usuario
 from ..gestion_pacientes.models import Cita
@@ -13,7 +18,14 @@ from ..gestion_expediente.models import Expediente
 class Home(TemplateView):
     template_name = 'gestion_pacientes/home.html'
 
-class verPerfil(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['citas'] = Cita.objects.filter(paciente_id_id=self.request.session['id_paciente'],
+                                               fecha=datetime.date.today()
+                                               ).order_by('hora_inicio')
+        return context
+
+class verPerfilPaciente(DetailView):
     template_name = 'gestion_pacientes/perfil.html'
     model = Paciente
     context_object_name = 'paciente'
@@ -27,4 +39,21 @@ class verExpediente(TemplateView):
         context['consultas'] = Consulta.objects.filter(expediente_id = self.kwargs.get('pk'))
         context['recetas'] = Receta.objects.filter()
         context['expediente'] = Expediente.objects.get(pk = self.kwargs.get('pk'))
+        return context
+        
+class cambiarContraseña(TemplateView):
+    template_name = 'gestion_pacientes/modcontraseña.html'
+
+class nuevaContraseña(TemplateView):
+    template_name = 'gestion_pacientes/nuevacontraseña.html'
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            usuario = Usuario.objects.get(curp = self.kwargs.get('pk'))
+            usuario.contrasenia = request.POST['newPass']
+            try:
+                usuario.save()
+                messages.success(request, 'Contraseña modificada correctamente')
+            except Exception as es:
+                messages.error(request, 'Hubo un error al intentar modificar la contraseña')
+        return redirect('homePaciente')
         
